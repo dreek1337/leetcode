f507f173525464a16bd834ec6003e55d480d6c96
dreek1337 <danil.sapog19@gmail.com>
2023-01-15 16:09:33 +0300

    Решение

--- solutions_num_1.py
+++
@@ -1,7 +0,0 @@
-# Contains Duplicate
-# https://leetcode.com/problems/contains-duplicate/
-
-class Solution:
-    def contains_duplicate(self, nums: list[int]) -> bool:
-
-        return not sorted(nums) == sorted(set(nums))
